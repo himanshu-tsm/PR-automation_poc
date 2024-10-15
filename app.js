84c0a04b1ea20e24911b752d1dac19b5f4ed35a0
himanshu-tsm <himanshu.tripathi@stackmentalist.com>
2024-10-15 22:43:46 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,4 +9,5 @@ const server = app.listen(3000, () => {
   console.log('Server running on port 3000');
 });
 
-module.exports = { app, server }; // Export the server instance
+// Export both app and server instances
+module.exports = { app, server };
