862e94905fe5c2c5f828a5a647442da41bc34ef3
himanshu-tsm <himanshu.tripathi@stackmentalist.com>
2024-10-15 22:45:22 +0530

    Update app.test.js

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,14 +1,13 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const app = require('../app'); // Assuming your app file is named 'app.js'
-const server = require('../app').server; // Modify to get the server instance
+const { app, server } = require('../app'); // Import both the app and server
 
 chai.use(chaiHttp);
 const expect = chai.expect;
 
 describe('App Tests', function() {
   it('should return Hello, PR Automation!', function(done) {
-    chai.request(app)
+    chai.request(server)  // Use the server instance here, not just app
       .get('/')
       .end((err, res) => {
         expect(res.text).to.equal('Hello, PR Automation!');
@@ -18,6 +17,6 @@ describe('App Tests', function() {
 
   // Add an after hook to close the server after tests
   after(function(done) {
-    server.close(done);  // Gracefully close the server
+    server.close(done);  // Gracefully close the server after tests
   });
 });
